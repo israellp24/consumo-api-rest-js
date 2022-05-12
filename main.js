67cc6eb2e9436c1c57acf9fb6c4a564aa3c99137
israellp24 <41647280+israellp24@users.noreply.github.com>
2022-05-12 00:04:43 -0500

    fin clase 9: GET

--- main.js
+++ main.js
@@ -1,38 +1,43 @@
-console.log("Hello word");
 
-const URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
+const API_Key = "6b1cd422-e33f-4bf6-9245-e4d69decfd2d";
+
+const API_URL_RANDOM = `https://api.thecatapi.com/v1/images/search?limit=2&api_key=${API_Key}`;
+
+const API_URL_FAVORITES = `https://api.thecatapi.com/v1/favourites/?limit=2&api_key=${API_Key}`;
+
 const btnReload = document.getElementById('Reload');
+const spanError = document.getElementById('error');
 
-/*
-fetch(URL)
-    .then(rest => rest.json())
-    .then(data=>{
-        const img = document.querySelector('img');
-        img.src= data[0].url;
-    });
-*/
+const loadRandomMichis = async()=>{
+    const response = await fetch(API_URL_RANDOM);
+    const data = await response.json();
+    console.log(data);
 
-const getDataCats = async () =>{
-    const response = await fetch(URL);
-    const json = await response.json();
-    return json;
+    if(response.status !== 200){
+        spanError.innerHTML= "Hubo un error: " + response.status;
+    }else{
+        const img1 = document.getElementById('imgCat1');
+        const img2 = document.getElementById('imgCat2');
+        img1.src=data[0].url;
+        img2.src=data[1].url;
+    }
+   
 }
 
-const setImgCat = async()=>{
-    const img1 = document.getElementById('imgCat1');
-    const img2 = document.getElementById('imgCat2');
-    const img3 = document.getElementById('imgCat3');
-    const response = await getDataCats();
-    console.log(response);
-    img1.src=response[0].url;
-    img2.src=response[1].url;
-    img3.src=response[2].url;
+const loadFavoritesMichis = async()=>{
+    const response = await fetch(API_URL_FAVORITES);
+    const data = await response.json();
+    console.log(data);
+    if(response.status !== 200){
+        spanError.innerHTML= "Hubo un error: " + response.status + " "+ data.message;
+    }
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    setImgCat();
+    loadRandomMichis();
+    loadFavoritesMichis();
 });
 
 btnReload.onclick = () =>{
-    setImgCat();
+    loadRandomMichis();
 }
